c7ac2b4846bcdbc8a159a52162c6be002474ab14
JunKyus <kuk941025@gmail.com>
2020-09-16 20:16:07 +0900

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Main from '@/pages/Main';
+import Navigation from '@/components/Navigation';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Main} />
-      </Switch>
+      <CssBaseline>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </CssBaseline>
     </BrowserRouter>
   );
 };
