b6e6ed071773f96e14b0102fd865bc61c92af9a9
JunKyus <kuk941025@gmail.com>
2020-09-16 11:00:41 +0900

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
