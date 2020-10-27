86b34917ad375600972cf298dc4a364713fa8e55
JunKyus <kuk941025@gmail.com>
2020-10-27 16:09:04 +0900

    Add title in display info component

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -18,24 +18,32 @@ const useStyles = makeStyles((theme) => ({
     flexShrink: 0,
     padding: theme.spacing(1),
   },
+  title: {
+    fontWeight: 'bold',
+  },
 }));
-const DisplayInfo = ({info}) => {
+const DisplayInfo = ({ info }) => {
   const { generation = 1, population = 1, bestScore = 1, avgScore = 1, mutationRate = 0.01, limit = 1000 } = info;
   const classes = useStyles();
   return (
-    <div className={classes.infoWrapper}>
-      <Typography className={classes.text}>generation</Typography>
-      <Typography className={classes.value}>{generation}</Typography>
-      <Typography className={classes.text}>population</Typography>
-      <Typography className={classes.value}>{population}</Typography>
-      <Typography className={classes.text}>Best Score</Typography>
-      <Typography className={classes.value}>{bestScore}</Typography>
-      <Typography className={classes.text}>Average Score</Typography>
-      <Typography className={classes.value}>{avgScore}</Typography>
-      <Typography className={classes.text}>Mutation Rate</Typography>
-      <Typography className={classes.value}>{mutationRate}</Typography>
-      <Typography className={classes.text}>Population limit</Typography>
-      <Typography className={classes.value}>{limit}</Typography>
+    <div>
+      <Typography gutterBottom className={classes.title} variant="h6">
+        Settings
+      </Typography>
+      <div className={classes.infoWrapper}>
+        <Typography className={classes.text}>generation</Typography>
+        <Typography className={classes.value}>{generation}</Typography>
+        <Typography className={classes.text}>population</Typography>
+        <Typography className={classes.value}>{population}</Typography>
+        <Typography className={classes.text}>Best Score</Typography>
+        <Typography className={classes.value}>{bestScore}</Typography>
+        <Typography className={classes.text}>Average Score</Typography>
+        <Typography className={classes.value}>{avgScore}</Typography>
+        <Typography className={classes.text}>Mutation Rate</Typography>
+        <Typography className={classes.value}>{mutationRate}</Typography>
+        <Typography className={classes.text}>Population limit</Typography>
+        <Typography className={classes.value}>{limit}</Typography>
+      </div>
     </div>
   );
 };
