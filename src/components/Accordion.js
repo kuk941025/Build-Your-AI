9c640c1a1b00d0c740d5d588b2c1c1a7c63ab974
JunKyus <kuk941025@gmail.com>
2020-10-27 15:29:54 +0900

    Refact or accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,6 +5,7 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,18 +20,36 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
   },
+  btnRoot: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(1),
+  },
+  detailWrapper: {
+    width: '100%',
+  },
 }));
 
-const CustomAccordion = ({ expanded, onClick, title = '', summary = '', children }) => {
+const CustomAccordion = ({ expanded, onExapnd, onSubmit, title = '', summary = '', children }) => {
   const classes = useStyles();
 
   return (
     <Accordion expanded={expanded}>
-      <AccordionSummary onClick={onClick} expandIcon={<ExpandMoreIcon />}>
+      <AccordionSummary onClick={onExapnd} expandIcon={<ExpandMoreIcon />}>
         <Typography className={classes.heading}>{title}</Typography>
         <Typography className={classes.secondaryHeading}>{summary}</Typography>
       </AccordionSummary>
-      <AccordionDetails>{children}</AccordionDetails>
+      <AccordionDetails>
+        <div className={classes.detailWrapper}>
+          {children}
+
+          <div className={classes.btnRoot}>
+            <Button onSubmit={onSubmit} color="primary" variant="contained">
+              Apply
+            </Button>
+          </div>
+        </div>
+      </AccordionDetails>
     </Accordion>
   );
 };
--- src/pages/Options.js
+++ src/pages/Options.js
@@ -18,7 +18,10 @@ const useStyles = makeStyles((theme) => ({
 const Options = () => {
   const classes = useStyles();
   const [scores, setScores] = useState(defaultScores);
-  const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState({
+    option: false,
+    genetics: false,
+  });
 
   const handleChange = (type) => (value) => {
     setScores({
@@ -27,23 +30,30 @@ const Options = () => {
     });
   };
 
+  const scoreOption = () =>
+    Object.keys(scores).map((type, idx) => (
+      <OptionText handleChange={handleChange(type)} text={type} value={scores[type]} key={idx} />
+    ));
+
   return (
     <div>
       <Accordion
         title="scores"
-        summary="cutomize score based on stone states"
-        expanded={expanded}
-        onClick={() => setExpanded((prev) => !prev)}>
-        <div className={classes.optionRoot}>
-          {Object.keys(scores).map((type, idx) => (
-            <OptionText handleChange={handleChange(type)} text={type} value={scores[type]} key={idx} />
-          ))}
+        summary="customize score based on stone states"
+        expanded={expanded.option}
+        onSubmit={() => updateScores(scores)}
+        onExapnd={() => setExpanded((prev) => ({ ...prev, option: !prev.option }))}>
+        <div className={classes.optionRoot}>{scoreOption()}</div>
+      </Accordion>
 
-          <div className={classes.btnRoot}>
-            <Button color="primary" onClick={() => updateScores(scores)} variant="contained">
-              Apply
-            </Button>
-          </div>
+      <Accordion
+        title="genetics"
+        summary="customize genetic related options"
+        onExapnd={() => setExpanded((prev) => ({ ...prev, genetics: !prev.genetics }))}
+        expanded={expanded.genetics}>
+        <div className={classes.optionRoot}>
+          <OptionText text="Mutation Rate" value={0.05} />
+          <OptionText text="Limit Population" value={10000} />
         </div>
       </Accordion>
     </div>
